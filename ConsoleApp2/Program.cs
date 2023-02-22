1bed03c1a85cfc799c52f2d96e07aba6c4871a86
Medi1975 <magdalenadobrowlanska@gmail.com>
2023-02-22 14:40:30 +0100

    Zadanie Dzień 4

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Ewa";
+string sex = "kobieta";
+int Age = 33;
+
+if (sex == "kobieta" && Age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+
+if (name == "Ewa" && Age > 30)
+{
+    Console.WriteLine("Ewa, lat 33.");
+}
+
+if (!(sex == "kobieta")) && (Age < 18);
+{
+    Console.WriteLine("Niepełnoletni mężczyzna.");
+}
